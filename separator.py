c7c2a70cdfc29f2ee8da65fde19b23d454c45638
Ashutosh Ranjan <ashutosh.ranjan019@gmail.com>
2017-10-21 16:38:36 +0530

    Add preprocessing file

---
+++ separator.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import pdb
+import sys
+
+#file_path = './test.txt'
+file_path = sys.argv[1] 
+#just for testing purpose
+TAIL_LIMIT = 2
+HEAD_LIMIT = 5
+
+def separator(file_path):
+    log = pd.read_table(file_path)
+    # print log.columns
+    ind=0
+    missing_clicks=pd.DataFrame(data=None, columns=log.columns)
+    head_tail_df = pd.DataFrame(data=None,columns=['Query','count','URL'] )
+    head_list = pd.DataFrame(data=None,columns=['Query','count','URL'] )
+    tail_list = pd.DataFrame(data=None,columns=['Query','count','URL'] )
+
+    for row in log.itertuples(index=False):
+
+        if pd.isnull(getattr(row,"ClickURL")): # Checkiing for the availabilty of url
+            missing_clicks=missing_clicks.append([row],ignore_index=True)
+            log.drop(log.index[ind])
+            continue
+
+        pos=head_tail_df.index[head_tail_df['Query'] == row[1]].tolist() #index for the query
+
+        if len(pos): #for increamenting the count of the query
+            head_tail_df.at[pos[0],head_tail_df.columns[1]]+=1
+            head_tail_df.at[pos[0],head_tail_df.columns[2]].append(getattr(row,"ClickURL")) #storing all urls visited at the given query
+        else: #adding new query
+            ins=[row[1],row[4],1]
+            head_tail_df=head_tail_df.append({'Query':row[1],'count':1,'URL':[getattr(row,"ClickURL")]},ignore_index=True)
+        ind+=1
+
+    for row in head_tail_df.itertuples(index=False):
+
+        # pdb.set_trace()
+        if row[1] < TAIL_LIMIT:
+            tail_list = tail_list.append({'Query':row[0],'count':row[1],'URL':row[2]},ignore_index=True)
+        elif row[1] > HEAD_LIMIT:
+            head_list = head_list.append({'Query':row[0],'count':row[1],'URL':row[2]},ignore_index=True)
+
+    return head_tail_df,missing_clicks,head_list,tail_list
+
+def main():
+    global file_path
+    head_tail_df,missing_clicks,head_list,tail_list = separator(file_path)
+    print head_list
+    print len(head_list),len(tail_list),len(head_tail_df),len(missing_clicks)
+
+
+if __name__ == "__main__":
+    main()
