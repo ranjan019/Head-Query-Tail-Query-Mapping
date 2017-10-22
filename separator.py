efcda2357a1fd1495f5f5b0439a1c80a23e3c27b
viditjain2119 <viditjain2119@gmail.com>
2017-10-22 18:39:06 +0530

    ItemRank added

--- separator.py
+++ separator.py
@@ -3,23 +3,24 @@ import pdb
 import sys
 
 #file_path = './test.txt'
-file_path = sys.argv[1] 
+file_path = sys.argv[1]
 #just for testing purpose
 TAIL_LIMIT = 2
 HEAD_LIMIT = 5
+MAX_RANK_ALLOWED = 5
 
 def separator(file_path):
     log = pd.read_table(file_path)
-    # print log.columns
+    print log.columns
     ind=0
     missing_clicks=pd.DataFrame(data=None, columns=log.columns)
-    head_tail_df = pd.DataFrame(data=None,columns=['Query','count','URL'] )
-    head_list = pd.DataFrame(data=None,columns=['Query','count','URL'] )
-    tail_list = pd.DataFrame(data=None,columns=['Query','count','URL'] )
+    head_tail_df = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
+    head_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
+    tail_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
 
     for row in log.itertuples(index=False):
 
-        if pd.isnull(getattr(row,"ClickURL")): # Checkiing for the availabilty of url
+        if pd.isnull(getattr(row,"ClickURL")) or getattr(row,"ItemRank")>MAX_RANK_ALLOWED: # Checkiing for the availabilty of url
             missing_clicks=missing_clicks.append([row],ignore_index=True)
             log.drop(log.index[ind])
             continue
@@ -29,18 +30,19 @@ def separator(file_path):
         if len(pos): #for increamenting the count of the query
             head_tail_df.at[pos[0],head_tail_df.columns[1]]+=1
             head_tail_df.at[pos[0],head_tail_df.columns[2]].append(getattr(row,"ClickURL")) #storing all urls visited at the given query
+            head_tail_df.at[pos[0],head_tail_df.columns[3]].append(getattr(row,"ItemRank"))
         else: #adding new query
             ins=[row[1],row[4],1]
-            head_tail_df=head_tail_df.append({'Query':row[1],'count':1,'URL':[getattr(row,"ClickURL")]},ignore_index=True)
+            head_tail_df=head_tail_df.append({'Query':row[1],'count':1,'URL':[getattr(row,"ClickURL")],'ItemRank':[getattr(row,"ItemRank")]},ignore_index=True)
         ind+=1
 
     for row in head_tail_df.itertuples(index=False):
 
         # pdb.set_trace()
         if row[1] < TAIL_LIMIT:
-            tail_list = tail_list.append({'Query':row[0],'count':row[1],'URL':row[2]},ignore_index=True)
+            tail_list = tail_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
         elif row[1] > HEAD_LIMIT:
-            head_list = head_list.append({'Query':row[0],'count':row[1],'URL':row[2]},ignore_index=True)
+            head_list = head_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
 
     return head_tail_df,missing_clicks,head_list,tail_list
 
