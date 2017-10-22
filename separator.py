4fda9b22ca5ea98d0966e3ac20eb6b7d44ad67d8
viditjain2119 <viditjain2119@gmail.com>
2017-10-22 21:51:32 +0530

    mapping done using set

--- separator.py
+++ separator.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pdb
 import sys
+from collections import defaultdict
 
 #file_path = './test.txt'
 file_path = sys.argv[1]
@@ -9,50 +10,108 @@ TAIL_LIMIT = 2
 HEAD_LIMIT = 5
 MAX_RANK_ALLOWED = 5
 
-def separator(file_path):
+def tryNew(file_path):
     log = pd.read_table(file_path)
-    print log.columns
-    ind=0
-    missing_clicks=pd.DataFrame(data=None, columns=log.columns)
-    head_tail_df = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
-    head_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
-    tail_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
-
+    query_Dict = defaultdict(int)
+    url_Dict = defaultdict(lambda : defaultdict(int))
+    url_head_Dict = defaultdict(lambda : defaultdict(int))
+    url_tail_Dict = defaultdict(lambda : defaultdict(int))
+    head_list = set()
+    tail_list = set()
     for row in log.itertuples(index=False):
-
         if pd.isnull(getattr(row,"ClickURL")) or getattr(row,"ItemRank")>MAX_RANK_ALLOWED: # Checkiing for the availabilty of url
-            missing_clicks=missing_clicks.append([row],ignore_index=True)
-            log.drop(log.index[ind])
             continue
+        query = getattr(row,"Query").lower()
+        url = getattr(row,"ClickURL")
+        rank = getattr(row,"ItemRank")
+        query_Dict[query] += 1
+        url_Dict[url][query] = rank
 
-        pos=head_tail_df.index[head_tail_df['Query'] == row[1]].tolist() #index for the query
+    for key in query_Dict.keys():
+        if query_Dict[key] > HEAD_LIMIT :
+            head_list.add(key)
+        elif query_Dict[key] < TAIL_LIMIT :
+            tail_list.add(key)
+    for key in url_Dict.keys():
+        for queryKey in url_Dict[key].keys():
+            if queryKey in head_list :# and queryKey not in tail_list :
+                url_head_Dict[key][queryKey] = url_Dict[key][queryKey]
+            elif queryKey in tail_list:
+                url_tail_Dict[key][queryKey] = url_Dict[key][queryKey]
+    return head_list,tail_list,url_head_Dict,url_tail_Dict
 
-        if len(pos): #for increamenting the count of the query
-            head_tail_df.at[pos[0],head_tail_df.columns[1]]+=1
-            head_tail_df.at[pos[0],head_tail_df.columns[2]].append(getattr(row,"ClickURL")) #storing all urls visited at the given query
-            head_tail_df.at[pos[0],head_tail_df.columns[3]].append(getattr(row,"ItemRank"))
-        else: #adding new query
-            ins=[row[1],row[4],1]
-            head_tail_df=head_tail_df.append({'Query':row[1],'count':1,'URL':[getattr(row,"ClickURL")],'ItemRank':[getattr(row,"ItemRank")]},ignore_index=True)
-        ind+=1
+#complete this function
+def findScore(rankTail,rankHead):
+    if rankTail==rankHead:
+        return (6-rankHead)
+    elif :
+        pass
 
-    for row in head_tail_df.itertuples(index=False):
+def mapping(head_list,tail_list,url_head_Dict,url_tail_Dict):
+    final_map = []
+    for tailKey in url_tail_Dict.keys():
+        for headKey in url_head_Dict[key].keys():
+            score = findScore(url_tail_Dict[tailKey],url_head_Dict[headKey])
+            final_map.append([tailKey,headKey,score])
+    return final_map
 
-        # pdb.set_trace()
-        if row[1] < TAIL_LIMIT:
-            tail_list = tail_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
-        elif row[1] > HEAD_LIMIT:
-            head_list = head_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
 
-    return head_tail_df,missing_clicks,head_list,tail_list
+
+
+# def separator(file_path):
+#     log = pd.read_table(file_path)
+#     print log.columns
+#     ind=0
+#     missing_clicks=pd.DataFrame(data=None, columns=log.columns)
+#     head_tail_df = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
+#     head_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
+#     tail_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
+#
+#     for row in log.itertuples(index=False):
+#
+#         if pd.isnull(getattr(row,"ClickURL")) or getattr(row,"ItemRank")>MAX_RANK_ALLOWED: # Checkiing for the availabilty of url
+#             missing_clicks=missing_clicks.append([row],ignore_index=True)
+#             log.drop(log.index[ind])
+#             continue
+#
+#         pos=head_tail_df.index[head_tail_df['Query'] == row[1]].tolist() #index for the query
+#
+#         if len(pos): #for increamenting the count of the query
+#             head_tail_df.at[pos[0],head_tail_df.columns[1]]+=1
+#             head_tail_df.at[pos[0],head_tail_df.columns[2]].append(getattr(row,"ClickURL")) #storing all urls visited at the given query
+#             head_tail_df.at[pos[0],head_tail_df.columns[3]].append(getattr(row,"ItemRank"))
+#         else: #adding new query
+#             ins=[row[1],row[4],1]
+#             head_tail_df=head_tail_df.append({'Query':row[1],'count':1,'URL':[getattr(row,"ClickURL")],'ItemRank':[getattr(row,"ItemRank")]},ignore_index=True)
+#         ind+=1
+#
+#     for row in head_tail_df.itertuples(index=False):
+#
+#         # pdb.set_trace()
+#         if row[1] < TAIL_LIMIT:
+#             tail_list = tail_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
+#         elif row[1] > HEAD_LIMIT:
+#             head_list = head_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
+#
+#     return head_tail_df,missing_clicks,head_list,tail_list
+
+# def getUrlList(head_list,tail_list):
+#     url_list = defaultdict()
+#     for row in head_list:
+#
+# def mapping(head_list,tail_list):
+
 
 def main():
     global file_path
-    head_tail_df,missing_clicks,head_list,tail_list = separator(file_path)
-    print head_list
-    print "taillist now"
-    print tail_list
-    print len(head_list),len(tail_list),len(head_tail_df),len(missing_clicks)
+    head_list,tail_list,url_head_Dict,url_tail_Dict = tryNew(file_path)
+    # print url_Dict
+    mapping(head_list,tail_list,url_head_Dict,url_tail_Dict)
+    # head_tail_df,missing_clicks,head_list,tail_list = separator(file_path)
+    # print head_list
+    # print "taillist now"
+    # print tail_list
+    # print len(head_list),len(tail_list),len(head_tail_df),len(missing_clicks)
 
 
 if __name__ == "__main__":
