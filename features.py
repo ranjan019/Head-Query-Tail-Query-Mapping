f34ae14f159d5c3452d95b0242a251770192fbbc
pulkit27bansal <pulkitbansal271192@gmail.com>
2017-10-23 10:35:31 +0530

    Features upload

---
+++ features.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+# calculating the head query features
+
+#head query embedding
+head_embedding=np.ndarray(shape=(3,), dtype=float, order='F')
+
+for row in head_list.itertuples(index=False):
+    head_query=getattr(row,"Query").replace('.',' ').split(" ")
+    
+    for word in head_query:
+        if word in model.wv.vocab:
+            head_embedding+=model[word]
+    
+    head_embedding/=len(head_query)
+    
+    print head_query,": ",head_embedding
+
+
+# calculating the tail query features
+
+
+#tail query embeding
+tail_embedding=np.ndarray(shape=(3,), dtype=float, order='F')
+
+for row in tail_list.itertuples(index=False):
+    tail_query=getattr(row,"Query").replace('.',' ').split(" ")
+    
+    for word in tail_query:
+        if word in model.wv.vocab:
+            tail_embedding+=model[word]
+        
+    tail_embedding/=len(tail_query) 
+    
+    print tail_query,": ",tail_embedding    
+
+
+# calculating the cosine similarity
+
+cosine_score=np.dot(head_embedding,tail_embedding)
+    
