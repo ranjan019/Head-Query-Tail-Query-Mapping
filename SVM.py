61663832295f22959a39a2075e47533194bacba8
viditjain2119 <viditjain2119@gmail.com>
2017-10-23 19:41:27 +0530

    Done reading:

--- SVM.py
+++ SVM.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn.svm import SVC
 import pydexter
 from sentencesimilarity
+from gensim.models import word2vec
 
 
 def makingQueryPairVector(query1, query2):
@@ -12,14 +13,16 @@ def makingQueryPairVector(query1, query2):
 	querypairvector.append(jcoef)
 	sentsim=calcQuerySimilarity(query1,query2)
 	querypairvector.append(sentsim)
- 
+	cosine_similarity = cosine_similarity(query1,query2,model)
+	querypairvector.append(cosine_similarity)
+
 
 
 X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
 y = np.array([1, 1, 2, 2])
 
 clf = SVC()
-clf.fit(X, y) 
+clf.fit(X, y)
 SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
     decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
@@ -32,4 +35,4 @@ for i in range(len(predict_Y)):
 	if predict_Y[i]==test_Y[i]:
 		precision+=1
 
-print "precision is: ",precision/len(predict_Y)		
+print "precision is: ",precision/len(predict_Y)
--- sentence-similarity.py
+++ sentence-similarity.py
@@ -1,49 +1,49 @@
 from nltk import word_tokenize, pos_tag
 from nltk.corpus import wordnet as wn
- 
+
 def penn_to_wn(tag):
     """ Convert between a Penn Treebank tag to a simplified Wordnet tag """
     if tag.startswith('N'):
         return 'n'
- 
+
     if tag.startswith('V'):
         return 'v'
- 
+
     if tag.startswith('J'):
         return 'a'
- 
+
     if tag.startswith('R'):
         return 'r'
- 
+
     return None
- 
+
 def tagged_to_synset(word, tag):
     wn_tag = penn_to_wn(tag)
     if wn_tag is None:
         return None
- 
+
     try:
         return wn.synsets(word, wn_tag)[0]
     except:
         return None
- 
+
 def sentence_similarity(sentence1, sentence2):
     """ compute the sentence similarity using Wordnet """
     # Tokenize and tag
     sentence1 = pos_tag(word_tokenize(sentence1))
     sentence2 = pos_tag(word_tokenize(sentence2))
-  
- 
+
+
     # Get the synsets for the tagged words
     synsets1 = [tagged_to_synset(*tagged_word) for tagged_word in sentence1]
     synsets2 = [tagged_to_synset(*tagged_word) for tagged_word in sentence2]
- 
+
     # Filter out the Nones
     synsets1 = [ss for ss in synsets1 if ss]
     synsets2 = [ss for ss in synsets2 if ss]
- 
+
     score, count = 0.0, 0
- 
+
     # For each word in the first sentence
     for synset in synsets1:
         # Get the similarity value of the most similar word in the other sentence
@@ -53,24 +53,23 @@ def sentence_similarity(sentence1, sentence2):
         	best_score = max([synset.path_similarity(ss) for ss in synsets2])
         else:
         	best_score = None
- 
+
         # Check that the similarity could have been computed
         if best_score is not None:
             score += best_score
             count += 1
- 
+
     # Average the values
     if(count>0):
     	score = score/count
     return score
- 
-query1="I live in NY."
-query2="I live with my parents."
+
+# query1="I live in NY."
+# query2="I live with my parents."
  
 def symmetric_sentence_similarity(sentence1, sentence2):
     """ compute the symmetric sentence similarity using Wordnet """
-    return (sentence_similarity(sentence1, sentence2) + sentence_similarity(sentence2, sentence1)) / 2 
- 
+    return (sentence_similarity(sentence1, sentence2) + sentence_similarity(sentence2, sentence1)) / 2
+
 def calQuerySimilarity(query1,query2):
     print symmetric_sentence_similarity(query1, query2)
-    
