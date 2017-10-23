126dab0a6945a5797c94713ff0551e633b369a34
pulkit27bansal <pulkitbansal271192@gmail.com>
2017-10-23 17:24:20 +0530

    upload

--- cosine-similarity.py
+++ cosine-similarity.py
@@ -1,42 +1,42 @@
 import numpy as np
 
-# calculating the head query features
+def cosine_similarity(headQuery,tailQuery,model):
+	# calculating the head query features
 
-#head query embedding
-head_embedding=np.ndarray(shape=(3,), dtype=float, order='F')
+	#head query embedding
+	head_embedding=np.ndarray(shape=(10,), dtype=float, order='F')
 
-for row in head_list.itertuples(index=False):
-    head_query=getattr(row,"Query").replace('.',' ').split(" ")
+	head_query=headQuery.replace('.',' ').split(" ")
     
-    for word in head_query:
-        if word in model.wv.vocab:
-            head_embedding+=model[word]
+    	for word in head_query:
+        	if word in model.wv.vocab:
+            		head_embedding+=model[word]
     
-    head_embedding/=len(head_query)
+    	head_embedding/=len(head_query)
     
-    #check if head_embedding is 0
-    print head_query,": ",head_embedding
+    	#check if head_embedding is 0
+    	#print head_query,": ",head_embedding
 
 
-# calculating the tail query features
+	# calculating the tail query features
 
+	#tail query embeding
+	tail_embedding=np.ndarray(shape=(10,), dtype=float, order='F')
 
-#tail query embeding
-tail_embedding=np.ndarray(shape=(3,), dtype=float, order='F')
-
-for row in tail_list.itertuples(index=False):
-    tail_query=getattr(row,"Query").replace('.',' ').split(" ")
+    	tail_query=tailQuery.replace('.',' ').split(" ")
     
-    for word in tail_query:
-        if word in model.wv.vocab:
-            tail_embedding+=model[word]
+    	for word in tail_query:
+        	if word in model.wv.vocab:
+            		tail_embedding+=model[word]
         
-    tail_embedding/=len(tail_query) 
+    	tail_embedding/=len(tail_query) 
     
-    #check if tail_embedding is 0
-    print tail_query,": ",tail_embedding    
+    	#check if tail_embedding is 0
+   	# print tail_query,": ",tail_embedding    
 
 
-# calculating the cosine similarity
-cosine_score=np.dot(head_embedding,tail_embedding)
+	# calculating the cosine similarity
+	cosine_score=np.dot(head_embedding,tail_embedding)
+
+	return cosine_score
     
