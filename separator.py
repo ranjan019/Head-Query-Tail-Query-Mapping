e20af176cedcc6051ca7b7ddabcfd1afd400439b
viditjain2119 <viditjain2119@gmail.com>
2017-11-09 21:38:28 +0530

    Creating final Data

--- separator.py
+++ separator.py
@@ -11,8 +11,8 @@ import pydexter
 file_path = sys.argv[1]
 #just for testing purpose
 TAIL_LIMIT = 5
-HEAD_LIMIT = 100
-MAX_RANK_ALLOWED = 5
+HEAD_LIMIT = 150
+MAX_RANK_ALLOWED = 3
 
 def calcJaccard(list1,list2):
     s1=set(list1)
@@ -57,7 +57,7 @@ def findScore(rankTail,rankHead):
     if rankTail<=5 and rankHead<=5:
         return 1
     else:
-        return -1
+        return 0
 
 
 def mapping(head_list,tail_list,url_head_Dict,url_tail_Dict):
@@ -84,74 +84,23 @@ def mapping(head_list,tail_list,url_head_Dict,url_tail_Dict):
                 coNeg += 1
     return final_map
 
-
-
-
-# def separator(file_path):
-#     log = pd.read_table(file_path)
-#     print log.columns
-#     ind=0
-#     missing_clicks=pd.DataFrame(data=None, columns=log.columns)
-#     head_tail_df = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
-#     head_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
-#     tail_list = pd.DataFrame(data=None,columns=['Query','count','URL','ItemRank'] )
-#
-#     for row in log.itertuples(index=False):
-#
-#         if pd.isnull(getattr(row,"ClickURL")) or getattr(row,"ItemRank")>MAX_RANK_ALLOWED: # Checkiing for the availabilty of url
-#             missing_clicks=missing_clicks.append([row],ignore_index=True)
-#             log.drop(log.index[ind])
-#             continue
-#
-#         pos=head_tail_df.index[head_tail_df['Query'] == row[1]].tolist() #index for the query
-#
-#         if len(pos): #for increamenting the count of the query
-#             head_tail_df.at[pos[0],head_tail_df.columns[1]]+=1
-#             head_tail_df.at[pos[0],head_tail_df.columns[2]].append(getattr(row,"ClickURL")) #storing all urls visited at the given query
-#             head_tail_df.at[pos[0],head_tail_df.columns[3]].append(getattr(row,"ItemRank"))
-#         else: #adding new query
-#             ins=[row[1],row[4],1]
-#             head_tail_df=head_tail_df.append({'Query':row[1],'count':1,'URL':[getattr(row,"ClickURL")],'ItemRank':[getattr(row,"ItemRank")]},ignore_index=True)
-#         ind+=1
-#
-#     for row in head_tail_df.itertuples(index=False):
-#
-#         # pdb.set_trace()
-#         if row[1] < TAIL_LIMIT:
-#             tail_list = tail_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
-#         elif row[1] > HEAD_LIMIT:
-#             head_list = head_list.append({'Query':row[0],'count':row[1],'URL':row[2],'ItemRank':row[3]},ignore_index=True)
-#
-#     return head_tail_df,missing_clicks,head_list,tail_list
-
-# def getUrlList(head_list,tail_list):
-#     url_list = defaultdict()
-#     for row in head_list:
-#
-# def mapping(head_list,tail_list):
-
-
 def main():
     global file_path
-
-    #configuring the word2vec
-    #sentences=word2vec.Text8Corpus('/home/pulkit/IIIT-H/NLP_Project/text8')
-    #model=word2vec.Word2Vec(sentences,size=10)
-
-    # creating dexter client connection
-    
-
     head_list,tail_list,url_head_Dict,url_tail_Dict = tryNew(file_path)
+    f = open('head.txt','w')
+    for key in head_list:
+        f.write(key)
+    f.close()
+    f = open('tail.txt','w')
+    for key in tail_list:
+        f.write(key)
+    f.close()
     maps = mapping(head_list,tail_list,url_head_Dict,url_tail_Dict)
+    f = open('mapping.txt','w')
     for row in maps:
-        print row[0],',',row[1],',',row[2]
-
-    #cosine_similarity(headQuery,tailQuery,model)
-    # head_tail_df,missing_clicks,head_list,tail_list = separator(file_path)
-    # print head_list
-    # print "taillist now"
-    # print tail_list
-    # print len(head_list),len(tail_list),len(head_tail_df),len(missing_clicks)
+        r = row[0],',,',row[1],',,',row[2]
+        f.write(r)
+    f.close()
 
 
 if __name__ == "__main__":
