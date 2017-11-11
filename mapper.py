8f401a50a6b6381cf75792e8170e7a5143292a2a
viditjain2119 <viditjain2119@gmail.com>
2017-11-11 14:56:08 +0530

    Mapper Done

---
+++ mapper.py
@@ -0,0 +1,40 @@
+import sys
+import gensim
+import keras
+import numpy as np
+
+HEAD_QUERY_PATH = './head150.txt'
+Word2vec_PATH = './w2vNet.model'
+word2vecLen = 100
+
+def wordTovec(headQuery,model):
+	head_embedding=np.ndarray(shape=(word2vecLen/2,), dtype=float, order='F')
+	head_query=headQuery.replace('.',' ').split(" ")
+	for word in head_query:
+		if word in model.wv.vocab:
+			head_embedding+=model[word]
+	head_embedding/=len(head_query)
+	return head_embedding
+
+def createHeadVectors(model):
+    head_queries = []
+    with open(HEAD_QUERY_PATH) as f:
+        for line in f:
+            head_queries.append(f.strip())
+    l1 = len(head_queries)
+    head_X = np.zeros((l1,word2vecLen),dtype=float)
+    for i in range(l1):
+        head_X[i] = wordTovec(head_queries[i],model)
+
+    return head_queries,head_X
+
+if __name__== "__main__":
+    model = gensim.models.Word2Vec.load(Word2vec_PATH)
+    tail_query = sys.argv[1]
+    tail_vector = wordTovec(tail_query,model)
+    neuralNetModel = load_model('my_model.h5')
+    head_queries , head_X = createHeadVectors(model)
+    l1 = len(head_X)
+    for i in range(l1):
+        test = [np.append(head_X[i],tail_vector)]
+        print neuralNetModel.predict(test)
